688cd68617f7e2620bdfd978d704035bbdabf43c
徐南木 <xunanmu@xunanmu.com>
2021-12-09 18:47:22 +0800

    👌feat(*): 基本工程结构搭建
    Documentation用来放文档
    include      此项目的接口函数
    lib          此项目生成的静态库文件
    src          此项目的核心源码
    log          日志库（https://github.com/armink/EasyLogger）在他上面修改了line输出格式
    test         测试库（https://github.com/ThrowTheSwitch/Unity）后期可能会改

---
+++ src/base_sandbox.c
@@ -0,0 +1,7 @@
+/**
+ * @author: 徐南木
+ * @date: 2021/12/9 16:52
+ * @description: 
+ */
+
+#include "base_sandbox.h"
---
+++ src/base_sandbox.h
@@ -0,0 +1,10 @@
+/**
+ * @author: 徐南木
+ * @date: 2021/12/9 16:52
+ * @description: 
+ */
+
+#ifndef SANDBOX_BASE_SANDBOX_H
+#define SANDBOX_BASE_SANDBOX_H
+
+#endif //SANDBOX_BASE_SANDBOX_H
