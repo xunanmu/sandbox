ef70542427a0ff19dade947f08ce241349d26516
徐南木 <xunanmu@xunanmu.com>
2022-01-20 15:30:45 +0800

    🎨feat(头文件): 改进一下网络配置结构体enum

--- src/base_sandbox.h
+++ src/base_sandbox.h
@@ -50,13 +50,13 @@ struct mnt {
 struct net{
     char *path;
     enum {
-        host,        /*主机模式，即与宿主机IP一样*/
-        bridge,      /*桥接模式，与宿主机处于同一个网段下*/
-        nat,         /*nat模式，即创建虚拟交换机，默认10.10.0.0*/
-        none,        /*无网模式，就存在一个lo本地环回设备*/
-    }net_mode;
-    unsigned char ipv4[4];   /*ipv4,ipv6默认可以不用设置，host，none设置无效，目前暂时不支持IPV6*/
-    unsigned char ipv6[];    /*bridge设置IP必须在同一网段下，nat设置IP若无交换机网段则重新创建虚拟交换机*/
+        host   = 0,         /*主机模式，即与宿主机IP一样*/
+        bridge = 1,         /*桥接模式，与宿主机处于同一个网段下*/
+        nat    = 2,         /*nat模式，即创建虚拟交换机，默认10.10.0.0*/
+        none   = 4,         /*无网模式，就存在一个lo本地环回设备*/
+    }net_mode;              /* nat|none,能实现类似VMware的主机模式*/
+    unsigned char ipv4[4];  /*ipv4,ipv6默认可以不用设置，host，none设置无效，目前暂时不支持IPV6*/
+    unsigned char ipv6[];   /*bridge设置IP必须在同一网段下，nat设置IP若无交换机网段则重新创建虚拟交换机*/
 };
 
 struct pid{
