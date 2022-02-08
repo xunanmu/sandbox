5c83e3a21ca964c34aa201a8ac8ad318f7416247
徐南木 <xunanmu@xunanmu.com>
2022-02-08 22:25:15 +0800

    ➕feat(*): 添加工具头文件utils.h

---
+++ src/utils.c
@@ -0,0 +1,7 @@
+/**
+ * @author: 徐南木
+ * @date: 2022/2/8 21:39
+ * @description: 
+ */
+
+#include "utils.h"
---
+++ src/utils.h
@@ -0,0 +1,10 @@
+/**
+ * @author: 徐南木
+ * @date: 2022/2/8 21:39
+ * @description: 
+ */
+
+#ifndef SANDBOX_UTILS_H
+#define SANDBOX_UTILS_H
+
+#endif //SANDBOX_UTILS_H
