e96ec0d0cd3ed16588a3b278a3cfaf2be9bb840a
徐南木 <xunanmu@xunanmu.com>
2022-02-19 15:52:58 +0800

    👌feat(*): 初始·化kolin sandbox 项目

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+import org.jetbrains.kotlin.utils.addToStdlib.ifFalse
+import java.net.URL
+import com.google.common.io.Files
+
+plugins {
+    kotlin("multiplatform") version "1.6.10"
+}
+
+group = "me.xunanmu"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+("sched.klib" in mkdir("lib").list()).ifFalse {
+    URL("https://gitee.com/xunanmu/klib/raw/master/linuxX64/sched.klib").apply {
+        Files.write(readBytes(),file("lib/sched.klib"))
+    }
+}
+
+kotlin {
+
+    linuxX64("native"){
+        binaries {
+            executable {
+                entryPoint = "main"
+            }
+        }
+    }
+    sourceSets {
+        val nativeMain by getting{
+            dependencies {
+                implementation(fileTree("lib"))
+            }
+        }
+        val nativeTest by getting
+    }
+}
