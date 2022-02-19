bf371287d37f7478c1e999329d3102a0b1e83feb
徐南木 <xunanmu@xunanmu.com>
2022-02-20 05:31:13 +0800

    🎨feat(com.xunanmu.logger):测试logger性能还是差

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,16 +1,12 @@
 import com.xunanmu.logger.*
 import logger.*
 import kotlin.system.measureNanoTime
-import kotlin.system.measureTimeMicros
-import kotlin.time.ExperimentalTime
-import kotlin.time.measureTime
-import kotlin.time.measureTimedValue
 
-@OptIn(ExperimentalTime::class)
+
 fun main() {
     val logger = LinuxX64Logger("test")
     LoggerConfig.loggerLevel = LoggerLevel.TRACE
-    println(measureTimedValue {
+    println(measureNanoTime {
         logger.error("这是个错误信息")
         logger.warn("这是一个警告信息")
         logger.info("这是一个普通消息")
@@ -22,7 +18,7 @@ fun main() {
         logger.debug("这是debug信息")
         logger.trace("这是一个追踪信息")
     })
-    println(measureTimedValue {
+    println(measureNanoTime {
         logger_output(LOGGER_ERROR, 0, null, null, "这是个错误信息%d", 1)
         logger_output(LOGGER_WARN, 0, null, null, "这是个错误信息%d", 2)
         logger_output(LOGGER_INFO, 0, null, null, "这是个错误信息%d", 3)
@@ -34,11 +30,18 @@ fun main() {
         logger_output(LOGGER_DEBUG, 0, null, null, "这是个错误信息%d", 4)
         logger_output(LOGGER_TRACE, 0, null, null, "这是个错误信息%d", 5)
     })
-    println(measureTimedValue{
+    println(measureNanoTime{
         logger_output(LOGGER_ERROR, 0, null, null, "这是个错误信息%d", 1)
         logger_output(LOGGER_WARN, 0, null, null, "这是个错误信息%d", 2)
         logger_output(LOGGER_INFO, 0, null, null, "这是个错误信息%d", 3)
         logger_output(LOGGER_DEBUG, 0, null, null, "这是个错误信息%d", 4)
         logger_output(LOGGER_TRACE, 0, null, null, "这是个错误信息%d", 5)
+    })
+    println(measureNanoTime{
+        logger.error("这是个错误信息")
+        logger.warn("这是一个警告信息")
+        logger.info("这是一个普通消息")
+        logger.debug("这是debug信息")
+        logger.trace("这是一个追踪信息")
     })
 }
--- src/nativeMain/kotlin/com/xunanmu/logger/LinuxX64Logger.kt
+++ src/nativeMain/kotlin/com/xunanmu/logger/LinuxX64Logger.kt
@@ -3,34 +3,52 @@ package com.xunanmu.logger
 import com.xunanmu.logger.LoggerLevel.*
 import com.xunanmu.logger.LoggerColor.*
 
-class LinuxX64Logger (val name: String){
+class LinuxX64Logger (val name: String)
 
+
+inline fun LinuxX64Logger.error(msg: String?){
+    println(formatMessage(ERROR,this.name,msg).brightRed)
 }
 
-inline fun LinuxX64Logger.error(msg: String?) {
-    if (ERROR.isLoggerEnabled()){
-        if (LoggerConfig.loggerColor){
-            println(formatMessage(ERROR,this.name,msg).brightRed)
-        }else{
-            println(formatMessage(ERROR,this.name,msg))
-        }
-    }
+inline fun LinuxX64Logger.warn(msg: String?){
+    println(formatMessage(WARN,this.name,msg).brightYellow)
 }
 
-inline fun LinuxX64Logger.warn(msg: String?) = WARN.logIfEnabled(msg,this.name,YELLOW)
-
-inline fun LinuxX64Logger.info(msg: String?) = INFO.logIfEnabled(msg,this.name,GREEN)
-
-inline fun LinuxX64Logger.debug(msg: String?) = DEBUG.logIfEnabled(msg,this.name,BLUE)
-
-inline fun LinuxX64Logger.trace(msg: String?) = TRACE.logIfEnabled(msg,this.name,PURPLE)
-
-inline fun LoggerLevel.logIfEnabled(msg :String?,loggerName: String,color: LoggerColor){
-    if (isLoggerEnabled()){
-        if (LoggerConfig.loggerColor){
-            println(color.setBrightColor(formatMessage(this,loggerName,msg)))
-        }else{
-            println(formatMessage(this,loggerName,msg))
-        }
-    }
+inline fun LinuxX64Logger.info(msg: String?){
+    println(formatMessage(INFO,this.name,msg).brightGreen)
 }
+
+inline fun LinuxX64Logger.debug(msg: String?){
+    println(formatMessage(DEBUG,this.name,msg).brightBlack)
+}
+
+inline fun LinuxX64Logger.trace(msg: String?){
+    println(formatMessage(TRACE,this.name,msg).brightPurple)
+}
+//inline fun LinuxX64Logger.error(msg: String?) {
+//    if (ERROR.isLoggerEnabled()){
+//        if (LoggerConfig.loggerColor){
+//            println(formatMessage(ERROR,this.name,msg).brightRed)
+//        }else{
+//            println(formatMessage(ERROR,this.name,msg))
+//        }
+//    }
+//}
+//
+//inline fun LinuxX64Logger.warn(msg: String?) = WARN.logIfEnabled(msg,this.name,YELLOW)
+//
+//inline fun LinuxX64Logger.info(msg: String?) = INFO.logIfEnabled(msg,this.name,GREEN)
+//
+//inline fun LinuxX64Logger.debug(msg: String?) = DEBUG.logIfEnabled(msg,this.name,BLUE)
+//
+//inline fun LinuxX64Logger.trace(msg: String?) = TRACE.logIfEnabled(msg,this.name,PURPLE)
+//
+//inline fun LoggerLevel.logIfEnabled(msg :String?,loggerName: String,color: LoggerColor){
+//    if (isLoggerEnabled()){
+//        if (LoggerConfig.loggerColor){
+//            println(color.setBrightColor(formatMessage(this,loggerName,msg)))
+//        }else{
+//            println(formatMessage(this,loggerName,msg))
+//        }
+//    }
+//}
