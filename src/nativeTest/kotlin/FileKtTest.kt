b59aa48cdd5651c7783ce51b5042cded049562e9
徐南木 <xunanmu@xunanmu.com>
2022-03-06 02:48:54 +0800

    ✅feat(com.xunanmu.utils.file.kt):createFile()

---
+++ src/nativeTest/kotlin/FileKtTest.kt
@@ -0,0 +1,20 @@
+import com.xunanmu.utils.createFile
+import com.xunanmu.utils.popen
+import kotlin.test.AfterTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class FileKtTest {
+    @Test
+    fun testCreateFile(): Unit {
+        if(createFile("test/test")){
+           assertEquals("test/test\n",popen("ls test/test"))
+        }
+        assertEquals(false, createFile("test/test/test"))
+    }
+
+    @AfterTest
+    fun deleteFile(){
+        popen("rm -rf test")
+    }
+}
